334b1e55f6a2165caaf28254d3967af1557fb4ec
m0chi/ <aly0mal99@gmail.com>
2025-02-15 18:46:16 +0100

    added taskhero component

---
+++ src/components/TaskHero.jsx
@@ -0,0 +1,14 @@
+function TaskHero({ tasks_completed, tasks_total }) {
+  return (
+    <section>
+      <div>
+        <p>Task Done</p>
+        <p>Keep it up</p>
+      </div>
+      <div>
+        {tasks_completed}/{tasks_total}
+      </div>
+    </section>
+  );
+}
+export default TaskHero;
