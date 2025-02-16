c7b14a1a59200d42c05fbfaa89994aaa18928b76
m0chi/ <aly0mal99@gmail.com>
2025-02-16 12:07:48 +0100

    tasks useState

--- src/app/page.js
+++ src/app/page.js
@@ -8,12 +8,18 @@ import TaskItem from "@/components/TaskItem";
 import TaskList from "@/components/TaskList";
 
 function Home() {
+  const [tasks, setTasks] = React.useState([]);
+
+  const tasks_completed = tasks.filter(
+    (task) => task.is_completed === true).length;
+  const tasks_total = tasks.length;
+
   return (
     <div className="wrapper">
       <Header />
       <TaskHero tasks_completed={0} tasks_total={0} />
-      <Form />
-      <TaskList tasks={[]} />
+      <Form setTasks={setTasks} />
+      <TaskList tasks={tasks} />
     </div>
   );
 }
