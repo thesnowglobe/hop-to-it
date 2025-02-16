7dc4e9dd073806cdccc0cbf087716d935a4e1827
m0chi/ <aly0mal99@gmail.com>
2025-02-16 12:49:04 +0100

    add props for mapping ListItem

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,10 +1,12 @@
 import TaskItem from "./TaskItem";
 
-function TaskList({ tasks }) {
+function TaskList({ tasks, setTasks }) {
   return (
     <ol className="task_list">
       {tasks && tasks.length > 0 ? (
-        tasks?.map((item, index) => <TaskItem key={index} item={item} />)
+        tasks?.map((item, index) => (
+          <TaskItem key={index} item={item} tasks={tasks} setTasks={setTasks} />
+        ))
       ) : (
         <p>Seems lonely in here, what are you up to?</p>
       )}
