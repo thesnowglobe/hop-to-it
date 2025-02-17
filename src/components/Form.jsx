385127af8bcb30abe5190f81e11d50ad92deaa0f
m0chi/ <aly0mal99@gmail.com>
2025-02-17 15:58:08 +0100

    add svg

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -27,14 +27,26 @@ function Form({ tasks, setTasks }) {
           type="text"
           name="task"
           id="task"
-          placeholder="Write your next task"
+          placeholder="Write your next task..."
         />
       </label>
       
       <button>
         <span className="visually-hidden">Submit</span>
-        <svg>
-          <path d="" />
+        <svg
+          clipRule="evenodd"
+          fillRule="evenodd"
+          strokeLinejoin="round"
+          strokeMiterlimit="2"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          width={32}
+          height={32}
+        >
+          <path
+            d="m11 11h-7.25c-.414 0-.75.336-.75.75s.336.75.75.75h7.25v7.25c0 .414.336.75.75.75s.75-.336.75-.75v-7.25h7.25c.414 0 .75-.336.75-.75s-.336-.75-.75-.75h-7.25v-7.25c0-.414-.336-.75-.75-.75s-.75.336-.75.75z"
+            fill="#bd8b99" stroke="#bd8b99"
+          />
         </svg>
       </button>
     </form>
